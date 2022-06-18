f6c8f854ee349e12a9fa0c11e82fe5ca554e5983
arunsinghrana <arunsingh15421@gmail.com>
2022-06-18 21:19:35 +0530

    on start method added

--- app/src/main/java/com/example/activitylifecycletest/MainActivity2.kt
+++ app/src/main/java/com/example/activitylifecycletest/MainActivity2.kt
@@ -2,10 +2,45 @@ package com.example.activitylifecycletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity2 : AppCompatActivity() {
+    val TAG = MainActivity2::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        Log.i(TAG,"onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG,"onRestart")
+    }
+
 }
